2232eadca406adede62a43088d993acab1f1cf1d
codechamp2006 <sagnikraychess@gmail.com>
2021-03-04 23:34:31 +0530

    copying some react files
    Just creating some boilerplate code

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
+import Fab from '@material-ui/core/Fab';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="Material-ui">
+      <Checkbox variant="contained" color="secondary"></Checkbox>
+      <Button variant="contained" color="secondary">
+      Hello World
+      </Button>
+      <Fab color="secondary" aria-label="edit">
+      </Fab>
+      </div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
